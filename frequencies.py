7763ec15d56da8e791fe9c5d0af233ff538f152d
Omar Ahmad <omar.1.ahmad@kcl.ac.uk>
2023-10-01 08:25:34 +0100

    first commit

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in range(len(items)):
+        item = str(items[i])
+        if item in frequencies:
+            frequencies[item] += 1
+        else:
+            frequencies[item] = 1
     return frequencies
